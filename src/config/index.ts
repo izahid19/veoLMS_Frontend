0a55ea0410f52ef6a83a271b584996fcafb9518f
zahid mushtaq <mushtaqzahid888@gmail.com>
2026-07-15 21:57:37 +0530

    small chnage

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,3 @@
-// export const API_URL = 'https://veolmsbackend-production.up.railway.app/api';
-export const API_URL = 'http://localhost:4001/api';
+export const API_URL = 'https://veolmsbackend-production.up.railway.app/api';
+//export const API_URL = 'http://localhost:4001/api';
 export const API_BASE_URL = API_URL;
